053af9271dac6fc3825da90e469d85e735738ce4
Solonaru Viorel <viorelsolonaru96@gmail.com>
2019-03-24 17:27:51 +0200

    Added database schema (back-end)

--- sms/src/main/java/sms/entities/item/pack/simplepack/SimplePackageService.java
+++ sms/src/main/java/sms/entities/item/pack/simplepack/SimplePackageService.java
@@ -1,38 +1,34 @@
 package sms.entities.item.pack.simplepack;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SimplePackageService implements ISimplePackageService {
 
-    @Autowired
-    private
-    ISimplePackageRepository simplePackageRepository;
+	@Autowired
+	private ISimplePackageRepository simplePackageRepository;
 
-    @Override
-    public Optional<SimplePackage> findSimplePackageById(int simplePackageId) {
-        return simplePackageRepository.findById(simplePackageId);
-    }
+	public Optional<SimplePackage> findSimplePackageById(int simplePackageId) {
+		return simplePackageRepository.findById(simplePackageId);
+	}
 
-    @Override
-    public List<SimplePackage> findAllSimplePackages() {
-        return (List<SimplePackage>) simplePackageRepository.findAll();
-    }
+	public List<SimplePackage> findAllSimplePackages() {
+		return (List<SimplePackage>) simplePackageRepository.findAll();
+	}
 
-    @Override
-    public void insertSimplePackage(SimplePackage simplePackage) {
-        simplePackageRepository.save(simplePackage);
-    }
+	public void insertSimplePackage(SimplePackage simplePackage) {
+		simplePackageRepository.save(simplePackage);
+	}
 
-    @Override
-    public void updateSimplePackage(SimplePackage simplePackage) {
-        simplePackageRepository.save(simplePackage);
-    }
+	public void updateSimplePackage(SimplePackage simplePackage) {
+		simplePackageRepository.save(simplePackage);
+	}
 
-    @Override
-    public void deleteSimplePackageById(int simplePackageId) {
-        simplePackageRepository.deleteById(simplePackageId);
-    }
+	public void deleteSimplePackageById(int simplePackageId) {
+		simplePackageRepository.deleteById(simplePackageId);
+	}
 }
